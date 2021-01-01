e0ae373da84c490de8c45a6f52e8c93ee26246fb
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-01-01 08:24:32 -0800

    added remove functionality for dynamic fields

--- app/javascript/controllers/nested_form_controller.js
+++ app/javascript/controllers/nested_form_controller.js
@@ -17,4 +17,20 @@ export default class extends Controller {
     );
     this.linksTarget.insertAdjacentHTML("beforebegin", content);
   }
+
+  remove_association(event) {
+    event.preventDefault();
+
+    let wrapper = event.target.closest("." + this.wrapperClass);
+
+    // New records are simply removed from the page
+    if (wrapper.dataset.newRecord == "true") {
+      wrapper.remove();
+
+      // Existing records are hidden and flagged for deletion
+    } else {
+      wrapper.querySelector("input[name*='_destroy']").value = 1;
+      wrapper.style.display = "none";
+    }
+  }
 }
